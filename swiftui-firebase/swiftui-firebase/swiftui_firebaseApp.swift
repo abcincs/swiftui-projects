d918f674f456d8953359689ce0cdcc43e9d0d8f1
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-05-06 23:52:06 +0200

    Finish Sample Proejct

--- swiftui-firebase/swiftui-firebase/swiftui_firebaseApp.swift
+++ swiftui-firebase/swiftui-firebase/swiftui_firebaseApp.swift
@@ -6,12 +6,25 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct swiftui_firebaseApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appdelegate
+    @StateObject var viewModel = AppViewModel()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(viewModel)
         }
+    }
+}
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
     }
 }
