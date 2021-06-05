d5d555f8a0e33cc2a473302c8c5a7569d009aab0
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-06-05 11:00:45 +0200

    Add Side Bar view from repl.it

---
+++ SwiftUIs/SwiftUIs/June/SIdeBarView.swift
@@ -0,0 +1,26 @@
+//
+//  SIdeBarView.swift
+//  SwiftUIs
+//
+//  Created by Cédric Bahirwe on 04/06/2021.
+//  Copyright © 2021 Cedric Bahirwe. All rights reserved.
+//
+
+import SwiftUI
+
+struct SIdeBarView: View {
+    var body: some View {
+        ZStack {
+            
+            Color.green
+            
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+struct SIdeBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        SIdeBarView()
+    }
+}
