f2294e6384cc79f4cf12a64e5748417dbfd1ce5a
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-06-25 10:00:48 +0200

    Composable Archi Testing

---
+++ CompsableArchitecture/CompsableArchitecture/CompsableArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompsableArchitectureApp.swift
+//  CompsableArchitecture
+//
+//  Created by Cédric Bahirwe on 25/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct CompsableArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
